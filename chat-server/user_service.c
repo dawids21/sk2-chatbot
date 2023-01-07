be9ee95b263061efc541490f45af29b05e184bf4
miloszmat123 <myyylek@gmail.com>
2023-01-07 14:14:39 +0100

    add and get messages updated, handle some warnings

--- chat-server/user_service.c
+++ chat-server/user_service.c
@@ -34,13 +34,12 @@ int get_user_id(char *token)
     {
         return 0;
     }
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
 
     char *sql = "SELECT user_id FROM logged_in_users WHERE token LIKE ?";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     
     int user_id = 0;
     if(stmt != NULL){
@@ -54,9 +53,8 @@ int get_user_id(char *token)
 
 cJSON *register_user(cJSON *request, int user_id, http_status *response_status)
 {
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
     
 
@@ -65,8 +63,8 @@ cJSON *register_user(cJSON *request, int user_id, http_status *response_status)
 
     char *sql = "SELECT COUNT(*) FROM users WHERE username LIKE ?";
 
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    int count;
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    int count = 0;
     if(stmt != NULL){
         sqlite3_bind_text(stmt, 1, username->valuestring, -1, SQLITE_TRANSIENT);
         sqlite3_step(stmt);
@@ -77,7 +75,7 @@ cJSON *register_user(cJSON *request, int user_id, http_status *response_status)
 
         sqlite3_stmt *stmt2;
         char *sql2 = "INSERT INTO users(username, password) VALUES(?, ?)";
-        rc = sqlite3_prepare_v2(db, sql2, -1, &stmt2, 0);
+        sqlite3_prepare_v2(db, sql2, -1, &stmt2, 0);
 
         
         if(stmt2 != NULL) {
@@ -89,9 +87,9 @@ cJSON *register_user(cJSON *request, int user_id, http_status *response_status)
 
         sqlite3_stmt *stmt3;
         char *sql3 = "SELECT user_id FROM users WHERE username LIKE ?";
-        rc = sqlite3_prepare_v2(db, sql3, -1, &stmt3, 0);
+        sqlite3_prepare_v2(db, sql3, -1, &stmt3, 0);
 
-        int user_id;
+        int user_id = 0;
         if(stmt3 != NULL) {
             sqlite3_bind_text(stmt3, 1, username->valuestring, -1, SQLITE_TRANSIENT);
             sqlite3_step(stmt3);
@@ -120,15 +118,13 @@ cJSON *login(cJSON *request, int user_id, http_status *response_status)
     cJSON *username = cJSON_GetObjectItemCaseSensitive(request, "username");
     cJSON *password = cJSON_GetObjectItemCaseSensitive(request, "password");
 
-
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
 
     char *sql = "SELECT COUNT(*) FROM users WHERE username LIKE ? AND password LIKE ?";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    int count;
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    int count = 0;
     if(stmt != NULL){
         sqlite3_bind_text(stmt, 1, username->valuestring, -1, SQLITE_TRANSIENT);
         sqlite3_bind_text(stmt, 2, password->valuestring, -1, SQLITE_TRANSIENT);
@@ -145,7 +141,7 @@ cJSON *login(cJSON *request, int user_id, http_status *response_status)
 
     sqlite3_stmt *stmt2;
     char *sql2 = "SELECT user_id FROM users WHERE username LIKE ?";
-    rc = sqlite3_prepare_v2(db, sql2, -1, &stmt2, 0);
+    sqlite3_prepare_v2(db, sql2, -1, &stmt2, 0);
     user_id = 0;
 
     if(stmt2 != NULL){
@@ -157,7 +153,7 @@ cJSON *login(cJSON *request, int user_id, http_status *response_status)
     if(user_id != 0){
         sqlite3_stmt *stmt3;
         char *sql3 = "DELETE FROM logged_in_users WHERE user_id = ?";
-        rc = sqlite3_prepare_v2(db, sql3, -1, &stmt3, 0);
+        sqlite3_prepare_v2(db, sql3, -1, &stmt3, 0);
             if(stmt3 != NULL){
                 sqlite3_bind_int(stmt3, 1, user_id);
                 sqlite3_step(stmt3);
@@ -170,7 +166,7 @@ cJSON *login(cJSON *request, int user_id, http_status *response_status)
 
     sqlite3_stmt *stmt4;
     char *sql4 = "INSERT INTO logged_in_users VALUES(?, ?);";
-    rc = sqlite3_prepare_v2(db, sql4, -1, &stmt4, 0);
+    sqlite3_prepare_v2(db, sql4, -1, &stmt4, 0);
     if(stmt4 != NULL){
         sqlite3_bind_int(stmt4, 1, user_id);
         sqlite3_bind_text(stmt4, 2, token, -1, SQLITE_TRANSIENT);
@@ -194,13 +190,12 @@ cJSON *get_users(cJSON *request, int user_id, http_status *response_status)
         return NULL;
     }
 
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
 
     char *sql = "SELECT user_id, username FROM users";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     cJSON *response_json = cJSON_CreateArray();
     if(stmt != NULL){
             while (sqlite3_step(stmt) != SQLITE_DONE){
@@ -223,15 +218,14 @@ cJSON *get_users_by_username(cJSON *request, int user_id, http_status *response_
         return NULL;
     }
 
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
 
     cJSON *username = cJSON_GetObjectItemCaseSensitive(request, "username");
 
     char *sql = "SELECT user_id, username FROM users WHERE username LIKE ?";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     cJSON *response_json = cJSON_CreateArray();
     if(stmt != NULL) {
         sqlite3_bind_text(stmt, 1, username->valuestring, -1, SQLITE_TRANSIENT);
@@ -262,14 +256,13 @@ cJSON *add_friend(cJSON *request, int user_id, http_status *response_status)
         return NULL;
     }
 
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
 
     cJSON *friend_id = cJSON_GetObjectItemCaseSensitive(request, "friend_id");
     char *sql = "INSERT INTO friends VALUES(?, ?)";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
     if(stmt != NULL) {
         sqlite3_bind_int(stmt, 1, user_id);
@@ -278,7 +271,7 @@ cJSON *add_friend(cJSON *request, int user_id, http_status *response_status)
         sqlite3_finalize(stmt);
     }
 
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
     if(stmt != NULL){
         sqlite3_bind_int(stmt, 2, user_id);
@@ -299,14 +292,13 @@ cJSON *delete_friend(cJSON *request, int user_id, http_status *response_status)
         return NULL;
     }
 
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
 
     cJSON *friend_id = cJSON_GetObjectItemCaseSensitive(request, "friend_id");
     char *sql = "DELETE FROM friends WHERE (user1_id = ? AND user2_id = ?) OR (user1_id = ? AND user2_id = ?)";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
     if(stmt != NULL) {
         sqlite3_bind_int(stmt, 1, user_id);
@@ -317,7 +309,7 @@ cJSON *delete_friend(cJSON *request, int user_id, http_status *response_status)
         sqlite3_finalize(stmt);
     }
 
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
     *response_status = HTTP_OK;
     return NULL;
@@ -330,13 +322,12 @@ cJSON *get_friends(cJSON *request, int user_id, http_status *response_status)
         *response_status = HTTP_UNAUTHORIZED;
         return NULL;
     }
-    int rc;
     sqlite3 *db;
-    rc = sqlite3_open("chat-db.db", &db);
+    sqlite3_open("chat-db.db", &db);
     sqlite3_stmt *stmt;
 
     char *sql = "SELECT user_id, username from users u, friends f WHERE f.user1_id = ? AND f.user2_id = u.user_id";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
    cJSON *response_json = cJSON_CreateArray();
     if(stmt != NULL){
@@ -360,18 +351,51 @@ cJSON *add_message(cJSON *request)
     cJSON *to = cJSON_GetObjectItemCaseSensitive(request, "to");
     cJSON *message = cJSON_GetObjectItemCaseSensitive(request, "message");
     cJSON *timestamp = cJSON_GetObjectItemCaseSensitive(request, "timestamp");
+
     int user_id = from->valueint;
     int friend_id = to->valueint;
     char *message_str = message->valuestring;
     char *timestamp_str = timestamp->valuestring;
-    printf("Add message %s at %s from %d to %d", message_str, timestamp_str, user_id, friend_id);
+
+    sqlite3 *db;
+    sqlite3_open("chat-db.db", &db);
+    sqlite3_stmt *stmt;
+
+    char *sql = "INSERT INTO messages(user_id, friend_id, message, timestamp) VALUES(?, ?, ?, ?)";
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+    if(stmt != NULL) {
+        sqlite3_bind_int(stmt, 1, user_id);
+        sqlite3_bind_int(stmt, 2, friend_id);
+        sqlite3_bind_text(stmt, 3, message_str, -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(stmt, 4, timestamp_str, -1, SQLITE_TRANSIENT);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+    }
+
+    sqlite3_stmt *stmt2;
+    char *sql2 = "SELECT message_id FROM messages WHERE user_id = ? AND friend_id = ? AND message LIKE ? AND timestamp LIKE ?";
+    sqlite3_prepare_v2(db, sql2, -1, &stmt2, 0);
+    int message_id = 0;
+
+    if(stmt2 != NULL) {
+        sqlite3_bind_int(stmt2, 1, user_id);
+        sqlite3_bind_int(stmt2, 2, friend_id);
+        sqlite3_bind_text(stmt2, 3, message_str, -1, SQLITE_TRANSIENT);
+        sqlite3_bind_text(stmt2, 4, timestamp_str, -1, SQLITE_TRANSIENT);
+        sqlite3_step(stmt);
+        message_id = sqlite3_column_int(stmt2, 0);
+        sqlite3_finalize(stmt);
+    }
+
 
     cJSON *message_json = cJSON_CreateObject();
-    cJSON_AddNumberToObject(message_json, "id", 8);
+    cJSON_AddNumberToObject(message_json, "id", message_id);
     cJSON_AddNumberToObject(message_json, "from", user_id);
     cJSON_AddNumberToObject(message_json, "to", friend_id);
     cJSON_AddStringToObject(message_json, "message", message_str);
     cJSON_AddStringToObject(message_json, "timestamp", timestamp_str);
+
     return message_json;
 }
 
@@ -384,23 +408,32 @@ cJSON *get_messages(cJSON *request, int user_id, http_status *response_status)
     }
 
     cJSON *friend_id = cJSON_GetObjectItemCaseSensitive(request, "friend_id");
-    printf("Get messages with friend %d\n", friend_id->valueint);
+
+    sqlite3 *db;
+    sqlite3_open("chat-db.db", &db);
+    sqlite3_stmt *stmt;
+
+    char *sql = "SELECT * FROM messages WHERE (friend_id = ? AND user_id = ?) OR (friend_id = ? AND user_id = ?)";
+    sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
     cJSON *response_json = cJSON_CreateArray();
-    cJSON *message_json = cJSON_CreateObject();
-    cJSON_AddNumberToObject(message_json, "id", 1);
-    cJSON_AddNumberToObject(message_json, "from", 1);
-    cJSON_AddNumberToObject(message_json, "to", 3);
-    cJSON_AddStringToObject(message_json, "message", "Siema");
-    cJSON_AddStringToObject(message_json, "timestamp", "2022-12-19T15:45:33");
-    cJSON_AddItemToArray(response_json, message_json);
-    message_json = cJSON_CreateObject();
-    cJSON_AddNumberToObject(message_json, "id", 2);
-    cJSON_AddNumberToObject(message_json, "from", 3);
-    cJSON_AddNumberToObject(message_json, "to", 1);
-    cJSON_AddStringToObject(message_json, "message", "Elo");
-    cJSON_AddStringToObject(message_json, "timestamp", "2022-12-19T15:52:41");
-    cJSON_AddItemToArray(response_json, message_json);
+    if(stmt != NULL){
+        sqlite3_bind_int(stmt, 1, friend_id -> valueint);
+        sqlite3_bind_int(stmt, 2, user_id);
+        sqlite3_bind_int(stmt, 3, user_id);
+        sqlite3_bind_int(stmt, 4, friend_id -> valueint);
+        while (sqlite3_step(stmt) != SQLITE_DONE){
+            cJSON *message_json = cJSON_CreateObject();
+            cJSON_AddNumberToObject(message_json, "id", sqlite3_column_int(stmt, 0));
+            cJSON_AddNumberToObject(message_json, "from", sqlite3_column_int(stmt, 1));
+            cJSON_AddNumberToObject(message_json, "to", sqlite3_column_int(stmt, 2));
+            cJSON_AddStringToObject(message_json, "message", sqlite3_column_text(stmt, 3));
+            cJSON_AddStringToObject(message_json, "timestamp", sqlite3_column_text(stmt, 4));
+            cJSON_AddItemToArray(response_json, message_json);
+        }
+    }
+    sqlite3_finalize(stmt);
+
     *response_status = HTTP_OK;
     return response_json;
 }
